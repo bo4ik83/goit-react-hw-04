d3d1b123be2e39b5e44a2952317f66bf503a6dfb
Denis Bochev <denisbo4ev@gmail.com>
2024-12-23 19:11:45 +0200

    add-loading

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import SearchBar from "../SearchBar/SearchBar.jsx";
 import ImageGallery from "../ImageGallery/ImageGallery.jsx";
+import Loader from "../Loader/Loader.jsx";
 import { searchImages } from "../../api/unsplashApi";
 
 const App = () => {
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = async (query) => {
+    setLoading(true);
     try {
       const data = await searchImages(query);
       setImages(data.results);
     } catch (error) {
       console.error("Error fetching images:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -19,6 +24,7 @@ const App = () => {
     <div>
       <SearchBar onSubmit={handleSearch} />
       <ImageGallery images={images} />
+      {loading && <Loader />}
     </div>
   );
 };
